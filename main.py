38589ef1da9c326965173479e89a36b30d485803
RyanMampilly <182558803+RyanMampilly@users.noreply.github.com>
2026-05-28 11:56:45 -0400

    Edited enemy spawn rates and updated intro screen

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Made by intern: @bassemfarid, no one or nothing else. 🤖
 """
 
 import pygame
+from random import randint
 
 # Initialize Pygame and create a window
 pygame.init()
@@ -14,23 +15,38 @@ clock = pygame.time.Clock()
 running = True  # Pygame main loop, kills pygame when False
 
 # Game state variables
-program_active = False # Rename of is_playing for clarity
+gameloop_active = False # Rename of is_playing for clarity
 GROUND_Y = 300  # The Y-coordinate of the ground level
 JUMP_START_SPEED = -20  # The speed at which the player jumps
 player_gravity_speed = 0
 score = 0
+spawner = pygame.USEREVENT + 1
+pygame.time.set_timer(spawner,randint(600,2000))
+enemy_list = []
+game_font = pygame.font.Font(pygame.font.get_default_font(), 50)
 
 # Initial screen
 screen.fill("White")
+title_surf  = game_font.render("EGG SPRINT!", False, "Black")
+title_rect  = title_surf.get_rect(center=(400,50))
+
+subtitle_surf  = pygame.font.Font(pygame.font.get_default_font(),20).render("Press <SPACE> or <MOUSE1> to start!", False, "Black")
+subtitle_rect  = subtitle_surf.get_rect(center=(400,350))
+
 player_surf = pygame.image.load("graphics/player/player_jump.png").convert_alpha()
+player_surf = pygame.transform.scale2x(player_surf)
 player_rect = player_surf.get_rect(center=(400,200))
+
 screen.blit(player_surf,player_rect)
+screen.blit(title_surf,title_rect)
+screen.blit(subtitle_surf,subtitle_rect)
+
 pygame.display.update()
 
-while not program_active:
+while not gameloop_active:
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            program_active = True
+        if (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE) or event.type == pygame.MOUSEBUTTONDOWN:
+            gameloop_active = True
         elif event.type == pygame.QUIT:
             pygame.quit()
 
@@ -38,7 +54,6 @@ while not program_active:
 # Load level assets
 SKY_SURF = pygame.image.load("graphics/level/sky.png").convert()
 GROUND_SURF = pygame.image.load("graphics/level/ground.png").convert()
-game_font = pygame.font.Font(pygame.font.get_default_font(), 50)
 
 score_surf = game_font.render("SCORE?", False, "Black")
 score_rect = score_surf.get_rect(center=(400, 50))
@@ -59,7 +74,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        elif program_active:
+        elif gameloop_active:
             # When player wants to jump by pressing SPACE
             if (
                 event.type == pygame.KEYDOWN
@@ -67,14 +82,17 @@ while running:
                 or event.type == pygame.MOUSEBUTTONDOWN
             ) and player_rect.bottom >= GROUND_Y:
                 player_gravity_speed = JUMP_START_SPEED
+            elif event.type == spawner:
+                enemy_list.append(egg_surf.get_rect(bottomleft=(800, GROUND_Y)))
+                pygame.time.set_timer(spawner,randint(600,2000))
         else:
             # When player wants to play again by pressing SPACE
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                program_active = True
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE or event.type == pygame.MOUSEBUTTONDOWN:
+                gameloop_active = True
                 egg_rect.left = 800
                 score = 0 # Reset score
 
-    if program_active:
+    if gameloop_active:
         screen.fill("purple")  # Wipe the screen
 
         # Update score
@@ -90,10 +108,11 @@ while running:
         screen.blit(score_surf, score_rect)
 
         # Adjust egg's horizontal location then blit it
-        egg_rect.x -= 5
-        if egg_rect.right <= 0:
-            egg_rect.left = 800
-        screen.blit(egg_surf, egg_rect)
+        for egg in enemy_list:
+            egg.x -= 5
+            if egg.right <= 0:
+                enemy_list.remove(egg)
+            screen.blit(egg_surf, egg)
 
         # Adjust player's vertical location then blit it
         player_gravity_speed += 1
@@ -103,14 +122,17 @@ while running:
         screen.blit(player_surf, player_rect)
 
         # When player collides with enemy, game ends
-        if egg_rect.colliderect(player_rect):
-            program_active = False
+        for egg in enemy_list:
+            if egg.colliderect(player_rect):
+                gameloop_active = False
+                enemy_list.clear()
+
 
     # When is over, display game over message
     else:
         screen.fill("black")
         screen.blit(game_over_surf, game_over_rect)
-        score_surf = game.font.render(f"SCORE: {score // 60}", False, "White") 
+        score_surf = game_font.render(f"SCORE: {score // 60}", False, "White") 
         screen.blit(score_surf, score_surf.get_rect(center=(400, 250)))
         
     # flip the display to put your work on screen
