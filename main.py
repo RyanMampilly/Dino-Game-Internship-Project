b825d7f00abfa277b7b8aa6787ebe4d61d962d55
RyanMampilly <182558803+RyanMampilly@users.noreply.github.com>
2026-05-29 14:10:22 -0400

    Added lives, sky scroll, game over animation

--- main.py
+++ main.py
@@ -24,6 +24,15 @@ spawner = pygame.USEREVENT + 1
 pygame.time.set_timer(spawner,randint(600,2000))
 enemy_list = []
 game_font = pygame.font.Font(pygame.font.get_default_font(), 50)
+lives = 3
+lives_bar = []
+
+def object_movement(object_surf, object_list):
+    for obj in object_list:
+        obj.x -= 5
+        if obj.right <= 0:
+            object_list.remove(obj)
+        screen.blit(object_surf, obj)
 
 # Initial screen
 screen.fill("White")
@@ -52,8 +61,8 @@ while not gameloop_active:
 
 
 # Load level assets
-SKY_SURF = pygame.image.load("graphics/level/sky.png").convert()
-GROUND_SURF = pygame.image.load("graphics/level/ground.png").convert()
+sky_surf = pygame.image.load("graphics/level/sky.png").convert()
+ground_surf = pygame.image.load("graphics/level/ground.png").convert()
 
 score_surf = game_font.render("SCORE?", False, "Black")
 score_rect = score_surf.get_rect(center=(400, 50))
@@ -67,6 +76,11 @@ player_rect = player_surf.get_rect(bottomleft=(25, GROUND_Y))
 
 egg_surf = pygame.image.load("graphics/egg/egg_1.png").convert_alpha()
 egg_rect = egg_surf.get_rect(bottomleft=(800, GROUND_Y))
+
+heart_f_surf = pygame.image.load("graphics/status/heart_full.png").convert_alpha()
+heart_b_surf = pygame.image.load("graphics/status/heart_broken.png").convert_alpha()
+for i in range(3):
+    lives_bar.append(heart_f_surf)
 
 while running:
     for event in pygame.event.get():
@@ -102,17 +116,16 @@ while running:
         score_surf = game_font.render(f"{score // 60}", False, "Black") 
         score_rect = score_surf.get_rect(center=(400, 50)) # Re-center text
 
+        # Scroll the sky to give a feeling of running instead of the eggs coming to you
+        sky_surf.scroll(-1,0,pygame.SCROLL_REPEAT)
+
         # Blit the level assets
-        screen.blit(SKY_SURF, (0, 0))
-        screen.blit(GROUND_SURF, (0, GROUND_Y))
+        screen.blit(sky_surf, (0, 0))
+        screen.blit(ground_surf, (0, GROUND_Y))
         screen.blit(score_surf, score_rect)
 
         # Adjust egg's horizontal location then blit it
-        for egg in enemy_list:
-            egg.x -= 5
-            if egg.right <= 0:
-                enemy_list.remove(egg)
-            screen.blit(egg_surf, egg)
+        object_movement(egg_surf, enemy_list)
 
         # Adjust player's vertical location then blit it
         player_gravity_speed += 1
@@ -121,11 +134,39 @@ while running:
             player_rect.bottom = GROUND_Y
         screen.blit(player_surf, player_rect)
 
+        # Blit hearts bar
+        for i in range(len(lives_bar)): screen.blit(lives_bar[i], (10+i*50,10))
+
         # When player collides with enemy, game ends
         for egg in enemy_list:
             if egg.colliderect(player_rect):
-                gameloop_active = False
-                enemy_list.clear()
+                lives -= 1
+                lives_bar[lives] = heart_b_surf
+                enemy_list.remove(egg)
+        if lives <= 0:
+            gameloop_active = False
+            # Game Over animation
+            player_gravity_speed = -10
+            player_surf = pygame.image.load("graphics/player/player_jump.png").convert_alpha()
+            while player_rect.bottom < 500:
+                player_gravity_speed += 0.5
+                player_rect.y += player_gravity_speed
+
+                screen.fill("purple")
+                screen.blit(sky_surf, (0, 0))
+                screen.blit(ground_surf, (0, GROUND_Y))
+                screen.blit(score_surf, score_rect)
+                for i in range(len(lives_bar)): screen.blit(lives_bar[i], (10+i*50,10))
+                for egg in enemy_list: screen.blit(egg_surf, egg)
+                screen.blit(player_surf, player_rect)
+
+                pygame.display.update()
+                clock.tick(60)
+
+            lives = 3
+            player_surf = pygame.image.load("graphics/player/player_walk_1.png").convert_alpha()
+            lives_bar = [heart_f_surf for i in lives_bar]
+            enemy_list.clear()
 
 
     # When is over, display game over message
